4cad76fbeac0809fcd49be05db67165d61270d4d
Michael K. Gilson <mgilson@ucsd.edu>
2019-06-25 17:55:48 -0700

    Edits to top of Results... more to come. (#55)
    * Edits to top of Results... more to come.
    
    * Further edits to results.
    
    * Further edits to results
    
    * Update 04.results.md
    
    * Update 04.results.md
    
    * Edits and markup to Discussion.
    
    It's still rough after my edits. Maybe we can work through these and then take a fresh look.
    
    * Resolve discrepancy with introduction
    
    * Add figures of H1-C2-C2-O2 dihedral
    
    * Add overall issue about table formatting
    
    * Simplify and update pseudodihedral figure
    
    * Update results & discussion

--- content/images/figures.sh
+++ content/images/figures.sh
@@ -184,3 +184,19 @@ alpha_gaff_charged_by_monomer.png \
 alpha_gaff_charged_whole_molecule.png \
 --geometry 2x1 \
 --output charge-schemes.png
+
+# Figure XX
+
+python ../../build/merge-subfigures.py --input \
+H1-C2-C2-O2-structure.png \
+SMIRNOFF99Frosst-vs-GAFF-v1.7-H1-C2-C2-O2.png \
+--geometry 2x1 \
+--output H1-C2-C2-O2.png
+
+# Figure XX
+
+python ../../build/merge-subfigures.py --input \
+pseudodihedral-histogram.png \
+pseudodihedral-timeseries.png \
+--geometry 2x1 \
+--output cyclodextrin-flexibility.png
